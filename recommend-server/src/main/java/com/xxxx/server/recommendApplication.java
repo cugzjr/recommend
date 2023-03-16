37e199d8d86fd17c85fc0839768a18a67d3ac049
zjr <745448680@qq.com>
2023-03-16 18:24:10 +0800

    Initial commit

---
+++ recommend-server/src/main/java/com/xxxx/server/recommendApplication.java
@@ -0,0 +1,11 @@
+package com.xxxx.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class recommendApplication {
+    public static void main(String[] args){
+        SpringApplication.run(recommendApplication.class, args);
+    }
+}
