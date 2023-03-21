4edf18450e4fd0bfb9585063f125896167b60b4b
zjr <745448680@qq.com>
2023-03-22 00:06:02 +0800

    11

--- recommend-server/src/main/java/com/xxxx/server/recommendApplication.java
+++ recommend-server/src/main/java/com/xxxx/server/recommendApplication.java
@@ -1,7 +1,9 @@
 package com.xxxx.server;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class recommendApplication {
